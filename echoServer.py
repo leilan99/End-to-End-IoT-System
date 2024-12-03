bc303064d95544f4b1963c59185902a38da2bf69
Leila Nunez <115498273+leilan99@users.noreply.github.com>
2024-12-03 01:49:36 -0800

    reorganized server into functions

--- echoServer.py
+++ echoServer.py
@@ -2,53 +2,65 @@
 
 import socket
 import ipaddress
+import errno
 
-# Print a message indicating the server is starting
-print("Starting server...")
-print()
+def set_port():
+    # Bind socket to the IP address and server port
+    while True:
+        try:
+            port = int(input("\nEnter port number: "))
 
-ip = '0.0.0.0'  # Setting IP address to listen on all available interfaces
+            if port < 0 or port>65535:
+                raise ValueError
+            else:
+                break
 
-# Create TCP socket
-TCPSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        except ValueError:
+            print("Port number not valid. Try again.") # Ensures user input matches declared format
+        
+def create_socket():
+    # Create TCP socket
+    return socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Bind socket to the IP address and server port
-while True:
-    try:
-        port = int(input("\nEnter port number: "))
 
-        if port < 0 or port>65535:
-            raise ValueError
-        else:
+def listen_tcp(ip, port):
+    TCPSocket = create_socket()
+    TCPSocket.bind((ip, port))
+    # Listen for incoming connections
+    TCPSocket.listen(5)
+    print(f"Server: {ip}:{port} - Listening...")
+    return TCPSocket
+
+def main():
+    # Print a message indicating the server is starting
+    print("Starting server...")
+    print()
+
+    ip = '0.0.0.0'  # Setting IP address to listen on all available interfaces
+
+    port = set_port()
+    TCPSocket = listen_tcp(ip, port)
+
+    # Accept an incoming connection
+    incomingSocket, incomingAddress = TCPSocket.accept()
+
+    # Loop to receive and respond to messages from the client
+    while True:
+        # Receive a message from the client (up to 1024 bytes)
+        clientMessage = incomingSocket.recv(1024)
+
+        # If no message is received, break the loop (client has disconnected)
+        if not clientMessage:
             break
 
-    except ValueError:
-        print("Port number not valid. Try again.") # Ensures user input matches declared format
+        # Display the client's message
+        print(f"Client message: {clientMessage.decode('utf-8')}")
+        
+        # Send the message back to the client, converted to uppercase
+        incomingSocket.send(clientMessage.upper())
 
-TCPSocket.bind((ip, port))
+    # Close the connection to the client
+    incomingSocket.close()
 
-
-# Listen for incoming connections
-TCPSocket.listen(5)
-print(f"Server: {ip}:{port} - Listening...")
-
-# Accept an incoming connection
-incomingSocket, incomingAddress = TCPSocket.accept()
-
-# Loop to receive and respond to messages from the client
-while True:
-    # Receive a message from the client (up to 1024 bytes)
-    clientMessage = incomingSocket.recv(1024)
-
-    # If no message is received, break the loop (client has disconnected)
-    if not clientMessage:
-        break
-
-    # Display the client's message
-    print(f"Client message: {clientMessage.decode('utf-8')}")
-    
-    # Send the message back to the client, converted to uppercase
-    incomingSocket.send(clientMessage.upper())
-
-# Close the connection to the client
-incomingSocket.close()
+if __name__ == "__main__":
+    main()
